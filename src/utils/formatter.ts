91fabe4bb297f0212dfcf9556b5f800f61b38a94
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-02-14 09:07:23 -0300

    add formatter

---
+++ src/utils/formatter.ts
@@ -0,0 +1,16 @@
+export function dateFormatter(date: string) {
+  return new Date(date).toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+}
+
+export function formatCurrency(value: number) {
+  const currency = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+
+  return currency;
+}
