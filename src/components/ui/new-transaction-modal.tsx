32f2b99f2a4ae988779dfb7aef503906ddf53683
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-02-14 09:48:55 -0300

    Add react hook form

--- src/components/ui/new-transaction-modal.tsx
+++ src/components/ui/new-transaction-modal.tsx
@@ -3,56 +3,106 @@ import * as Radio from "@radix-ui/react-radio-group";
 import { ArrowCircleUp, X } from "phosphor-react";
 
 import cn from "../../utils/cn";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface INewTransactionModalProps {}
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(["income", "outcome"]),
+});
 
-export function NewTransactionModal({}: INewTransactionModalProps) {
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
+
+export function NewTransactionModal() {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+    control,
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      description: "",
+      price: 0,
+      category: "",
+      type: "income",
+    },
+  });
+
+  function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    console.log(data);
+  }
+
   return (
     <>
       <Dialog.Portal>
         <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
-        <Dialog.Content className="min-w-[32rem] bg-gray-800 py-10 px-12 rounded-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Dialog.Content className="min-w-[32rem] bg-gray-800 py-10 px-12 rounded-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-y-3">
           <Dialog.Title className="text-white font-bold text-[1.25rem]">
             Nova Transação
           </Dialog.Title>
+          <Dialog.DialogDescription className="text-gray-300 text-sm">
+            Preencha os campos abaixo para criar uma nova transação.
+          </Dialog.DialogDescription>
 
-          <form action="" className="flex flex-col mt-8 gap-4">
+          <form
+            onSubmit={handleSubmit(handleCreateNewTransaction)}
+            className="flex flex-col mt-8 gap-4"
+          >
             <input
               className="rounded-md border-0 bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
               type="text"
               placeholder="Descrição"
               required
+              {...register("description")}
             />
             <input
               className="rounded-md border-0 bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
               type="text"
               placeholder="Preço"
               required
+              {...register("price", { valueAsNumber: true })}
             />
             <input
               className="rounded-md border-0 bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
               type="text"
               placeholder="Categoria"
               required
+              {...register("category")}
             />
 
-            <Radio.Root className="grid grid-cols-2 gap-4 mt-2 focus:outline-none">
-              <TransactionTypeButton
-                variant="income"
-                label="Entrada"
-                value="income"
-              />
-              <TransactionTypeButton
-                variant="outcome"
-                label="Saída"
-                value="outcome"
-              />
-            </Radio.Root>
-
+            <Controller
+              control={control}
+              name="type"
+              render={({ field }) => {
+                return (
+                  <Radio.Root
+                    className="grid grid-cols-2 gap-4 mt-2 focus:outline-none"
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <TransactionTypeButton
+                      variant="income"
+                      label="Entrada"
+                      value="income"
+                    />
+                    <TransactionTypeButton
+                      variant="outcome"
+                      label="Saída"
+                      value="outcome"
+                    />
+                  </Radio.Root>
+                );
+              }}
+            />
             <button
               className="h-[58px] border-0 bg-green-500 text-white font-bold rounded-md px-5 mt-6
-            cursor-pointer hover:bg-green-700 transition-colors "
+            cursor-pointer hover:bg-green-700 transition-colors disabled:opacity-70 disabled:cursor-not-allowed"
               type="submit"
+              disabled={isSubmitting}
             >
               Cadastrar
             </button>
--- src/pages/transactions/components/search-form.tsx
+++ src/pages/transactions/components/search-form.tsx
@@ -1,21 +1,58 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-interface ISearchFormProps {}
+const searchFormSchema = z.object({
+  query: z.string(),
+});
 
-export function SearchForm({}: ISearchFormProps) {
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
+
+export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+    defaultValues: {
+      query: "",
+    },
+  });
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2000);
+    });
+
+    console.log(data);
+  }
+
   return (
     <>
-      <form className="flex gap-4 ">
+      <form
+        onSubmit={handleSubmit(handleSearchTransactions)}
+        className="flex gap-4 "
+      >
         <input
           placeholder="Busque por transações"
           className="flex-1 rounded-md border-0 bg-gray-900 p-4 placeholder:text-gray-500 text-gray-300 "
+          {...register("query")}
         />
         <button
-          className="flex items-center gap-3 border-[1px] border-green-300 text-green-300 font-bold 
+          disabled={isSubmitting}
+          className="flex items-center gap-3 border-[1px] border-green-300 text-green-300 font-bold disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-green-300
         rounded-md bg-transparent  p-4 hover:bg-green-500 hover:text-white hover:border-green-500 transition-colors duration-200"
         >
-          <MagnifyingGlass size={20} />
-          Buscar
+          {isSubmitting ? (
+            <div className="rounded-full animate-spin border-2 border-white   w-4 h-4"></div>
+          ) : (
+            <div className="flex items-center gap-3">
+              <MagnifyingGlass size={20} />
+              Buscar
+            </div>
+          )}
         </button>
       </form>
     </>
