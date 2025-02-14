67c06fb919be19c08cca4302a3d16034fe366bfe
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-02-14 08:39:57 -0300

    add forwardRef

--- src/components/ui/new-transaction-button.tsx
+++ src/components/ui/new-transaction-button.tsx
@@ -1,20 +1,22 @@
-interface INewTransactionButtonProps
-  extends React.HTMLAttributes<HTMLButtonElement> {
-  label: string;
-}
+import { forwardRef } from "react";
 
-export function NewTransactionButton({
-  label,
-  ...props
-}: INewTransactionButtonProps) {
+type TNewTransactionButtonProps = React.ComponentProps<"button"> & {
+  label: string;
+};
+
+export const NewTransactionButton = forwardRef<
+  HTMLButtonElement,
+  TNewTransactionButtonProps
+>(({ label, ...props }, ref) => {
   return (
-    <>
-      <button
-        {...props}
-        className="h-12 bg-green-500 border-0 font-bold rounded-md flex items-center justify-center text-white pointer hover:bg-green-700 px-6"
-      >
-        {label}
-      </button>
-    </>
+    <button
+      {...props}
+      ref={ref}
+      className="h-12 bg-green-500 border-0 font-bold rounded-md flex items-center justify-center text-white pointer hover:bg-green-700 px-6"
+    >
+      {label}
+    </button>
   );
-}
+});
+
+NewTransactionButton.displayName = "NewTransactionButton";
